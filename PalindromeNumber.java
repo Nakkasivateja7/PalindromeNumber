69cd6585aad8d4e531071295344650ec81d96068
coder <158988176+Nakkasivateja7@users.noreply.github.com>
2024-02-13 23:17:11 +0530

    Add files via upload
    Given string/number is Palindrome or not.

---
+++ PalindromeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class PalindromeNumber {
+   public static void main(String args[]) {
+      Scanner Obj = new Scanner(System.in);   
+      System.out.println("Enter the number of strings/numbers to check if they are palindromes");  
+      int n = Obj.nextInt();
+      Obj.nextLine(); // consume newline not consumed by nextInt()
+      String original[] = new String[n];
+      for(int i=0;i<n;i++){
+         System.out.println("Enter string/number " + (i+1) + ":");
+         original[i] = Obj.nextLine();   
+      }
+      for(int i=0;i<n;i++){
+         String reverse = new StringBuilder(original[i]).reverse().toString();  
+         if (original[i].equals(reverse))  
+            System.out.println("Entered string/number " + (i+1) + " is a palindrome.");  
+         else  
+            System.out.println("Entered string/number " + (i+1) + " isn't a palindrome."); 
+            Obj.close();
+      }
+   }  
+}
